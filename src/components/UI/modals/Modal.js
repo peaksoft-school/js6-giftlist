bd6544ff641dd2c1017ffef17e28692a034f79c6
Bektur12 <kanybekovBektur@gmail.com>
2022-10-06 16:46:35 +0600

    addModal

--- src/components/UI/modals/Modal.js
+++ src/components/UI/modals/Modal.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react'
+import { Modal as MUIModal } from '@mui/material'
+import styled from 'styled-components'
 
-function Modal() {
-  return <div>
-    
-  </div>;
+function Modal({ isOpen, onClose, children }) {
+   return (
+      <MUIModal open={isOpen} onClose={onClose}>
+         <CenteredContainer>{children}</CenteredContainer>
+      </MUIModal>
+   )
 }
 
-export default Modal;
+export default Modal
+
+const CenteredContainer = styled.div`
+   position: absolute;
+   left: 50%;
+   top: 50%;
+   transform: translate(-50%, -50%);
+`

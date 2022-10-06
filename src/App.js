a5bb1aacc73594b2584f4f4c4f19f18452a74919
Erlan <erlan.bakalov98@gmail.com>
2022-10-06 15:39:51 +0600

    Gift Card is ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css'
+import GiftCard from './components/UI/GiftCard'
+import userimage from './assets/images/bookedCards/userImage.png'
+import userspost from './assets/images/bookedCards/userPost.png'
+import points from './assets/images/bookedCards/points.svg'
 
 function App() {
    return (
       <div className="App">
-         <h1>hello</h1>
+         <GiftCard
+            usersName="Erlan"
+            postName="Письма Элджертона"
+            newGift="Новый"
+            userImage={userimage}
+            userPost={userspost}
+            postDate="06.10.22"
+            booked="Забронтрован"
+            vector={points}
+         />
+         <GiftCard
+            usersName="Erlan"
+            postName="Письма Элджертона"
+            newGift="Новый"
+            userImage={userimage}
+            userPost={userspost}
+            postDate="06.10.22"
+            booked="Забронтрован"
+            vector={points}
+         />
+         <GiftCard
+            usersName="Erlan"
+            postName="Письма Элджертона"
+            newGift="Новый"
+            userImage={userimage}
+            userPost={userspost}
+            postDate="06.10.22"
+            booked="Забронтрован"
+            vector={points}
+         />
       </div>
    )
 }

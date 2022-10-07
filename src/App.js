c5ddff0124290efdd6812238ade29677d58a8e99
Bektur12 <kanybekovBektur@gmail.com>
2022-10-07 20:05:04 +0600

    clear App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css'
-import Menu from './components/UI/meadballs/Menu'
 
 function App() {
-   return (
-      <div className="App">
-         <Menu />{' '}
-      </div>
-   )
+   return <div className="App"> </div>
 }
 
 export default App

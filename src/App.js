3d11b941c200627edb62d0636bd176d8e213b84d
Bektur12 <kanybekovBektur@gmail.com>
2022-10-07 16:33:21 +0600

    clear-trash

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import './App.css'
-import Modal from './components/UI/modals/Modal'
 
 function App() {
-   const open = true
-   return (
-      <div className="App">
-         <Modal isOpen={open}>
-            <div>hello</div>
-            <div>hello</div>
-            <div>hellавыфавыфавфыавыфавыфавыфавыфавыфавыфo</div>
-            <div>hello</div>
-            <div>hello</div>
-            <div>helвыфваыфаыфаывавываываыlo</div>
-            <div>hello</div>
-            <button>withа</button>
-         </Modal>
-      </div>
-   )
+   return <div className="App"> </div>
 }
 
 export default App

aa859ce55789d2ba6b1f6230b539057932e78cd3
Erlan <erlan.bakalov98@gmail.com>
2022-10-08 02:41:51 +0600

    fixed/oneCard  and finished seconCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css'
-import GiftCard from './components/UI/GiftCard'
 
 function App() {
-   return (
-      <div className="App">
-         <GiftCard />
-      </div>
-   )
+   return <div className="App"> </div>
 }
 
 export default App

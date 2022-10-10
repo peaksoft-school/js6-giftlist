df8b1981e32b9ee71f9277fa735ca9f8f9abfe82
Bektur12 <kanybekovBektur@gmail.com>
2022-10-11 00:23:57 +0600

    fixed/meadballs

--- src/components/UI/meatballs/Menu.js
+++ src/components/UI/meatballs/Menu.js
@@ -3,22 +3,22 @@ import { Menu as MUIMenu } from '@mui/material'
 import MenuItem from './MenuItem'
 import ControllsMenu from './ControllsMenu'
 
-function Menu({ children }) {
+function Menu(options = []) {
    const [anchorEl, setAnchorEl] = React.useState(null)
+
    const open = Boolean(anchorEl)
 
-   const handleClick = (event) => {
-      setAnchorEl(event.currentTarget)
-   }
-   const handleClose = () => {
-      setAnchorEl(null)
-   }
+   const handleClick = (event) => setAnchorEl(event.currentTarget)
+
+   const handleClose = () => setAnchorEl(null)
 
    return (
       <div>
          <ControllsMenu isOpen={handleClick} />
          <MUIMenu anchorEl={anchorEl} open={open} onClose={handleClose}>
-            <MenuItem>{children}</MenuItem>
+            {options.map((option) => (
+               <MenuItem>{option}</MenuItem>
+            ))}
          </MUIMenu>
       </div>
    )

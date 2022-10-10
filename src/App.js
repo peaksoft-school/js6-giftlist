2da1ae88e8f3fed13119869c1c186a39db1aed20
Erlan <erlan.bakalov98@gmail.com>
2022-10-10 18:42:41 +0600

    deleted component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css'
-import GiftCard from './components/UI/GiftCard'
 
 function App() {
-   return (
-      <div className="App">
-         <GiftCard />
-      </div>
-   )
+   return <div className="App">gift</div>
 }
 
 export default App

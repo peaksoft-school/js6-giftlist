f9250b89c998eaf1c011bdd8f4c59dbb972c1646
mirlan21 <mirlan20010180@gmail.com>
2022-10-11 12:55:59 +0600

    Create Search input component

--- src/components/UI/SearchInput.js
+++ src/components/UI/SearchInput.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
-import IconSearch from '../../assets/images/Union.svg'
+import iconSearch from '../../assets/images/Union.svg'
 
-const SearchInput = () => {
+const SearchInput = ({ value, inputChangeHandler }) => {
    return (
       <div>
          <InputBlock>
-            <img src={IconSearch} alt="" />
-            <Input type="text" />
+            <Icon src={iconSearch} alt="" />
+            <Input
+               onChange={inputChangeHandler}
+               value={value}
+               type="text"
+               placeholder="Поиск"
+            />
          </InputBlock>
       </div>
    )
@@ -17,11 +22,28 @@ export default SearchInput
 
 const Input = styled.input`
    outline: none;
-   width: 100%;
+   border: 0;
+   width: 743px;
+   height: 19px;
+   margin: 10px 18px 10px 0px;
+   ::placeholder {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 19px;
+   }
 `
 
 const InputBlock = styled.div`
-   width: 200px;
-   border: 1px solid black;
+   width: 821px;
+   height: 40px;
+   border: 1px solid #bdbdbd;
+   border-radius: 8px;
    display: flex;
+`
+const Icon = styled.img`
+   width: 20px;
+   height: 20px;
+   margin: 10px 20px 10px 20px;
 `

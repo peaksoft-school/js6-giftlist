70861cb255521a95bca11e5c9c44b7dcc572d88a
mirlan21 <mirlan20010180@gmail.com>
2022-10-11 15:43:50 +0600

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import CheckBox from './components/UI/checkBox'
 
 function App() {
-   return <div className="App"> </div>
+   return (
+      <div className="App">
+         <CheckBox />
+      </div>
+   )
 }
 
 export default App
---
+++ src/components/UI/checkBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckBox() {
+   return (
+      <div>
+         <input type="checkbox" />
+      </div>
+   )
+}
+
+export default CheckBox

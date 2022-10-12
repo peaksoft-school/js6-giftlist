5e687a5333c543efa3d5a3d14f00f008cbb54102
mirlan21 <mirlan20010180@gmail.com>
2022-10-12 17:28:56 +0600

    create check-box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import './App.css'
-import CheckBox from './components/UI/checkBox'
 
 function App() {
-   return (
-      <div className="App">
-<<<<<<< HEAD
-         <CheckBox />
-=======
-         <h1>hello</h1>
->>>>>>> 035c86b852059aa51e9871677b96d9402eabacd6
-      </div>
-   )
+   return <div className="App" />
 }
 
 export default App
--- src/components/UI/checkBox.js
+++ src/components/UI/checkBox.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import styled from 'styled-components'
 
-function CheckBox() {
+function CheckBox({ checked, onChange, ...other }) {
    return (
       <div>
-         <input type="checkbox" />
+         <Input
+            {...other}
+            type="Checkbox"
+            onChange={onChange}
+            checked={checked}
+         />
       </div>
    )
 }
 
 export default CheckBox
+const Input = styled.input`
+   appearance: none;
+   height: 15px;
+   width: 15px;
+   border: 1.6px solid #87898e;
+   border-radius: 4px;
+   cursor: pointer;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   outline: none;
+   &[type='Checkbox']::after {
+      font-family: 'Font Awesome 5 Free';
+      font-weight: 900;
+      width: 16px;
+      font-size: 9px;
+      color: white;
+      content: '\f00c';
+      background-color: #8639b5;
+      border-color: #8639b5;
+      border-radius: 6px;
+      display: none;
+   }
+   &[type='Checkbox']:hover {
+      border-color: #8639b5;
+   }
+   &[type='Checkbox']:checked {
+      background-color: #8639b5;
+      border-color: #8639b5;
+   }
+   &[type='Checkbox']:checked:after {
+      display: block;
+      background-color: block;
+      width: 9px;
+   }
+   &:disabled {
+      border-color: #0000001f;
+   }
+   &:disabled:hover {
+      border-color: #0000001f;
+   }
+`

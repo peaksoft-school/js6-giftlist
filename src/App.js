6517ff1a8a279253b52d709cd6c9aa423998c8d6
Erlan <erlan.bakalov98@gmail.com>
2022-10-14 15:37:58 +0600

    fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css'
-import Inputs from './components/UI/Inputs'
 
 function App() {
-   return (
-      <div className="App">
-         <Inputs placeholder="noooooo" />
-      </div>
-   )
+   return <div className="App"> </div>
 }
 
 export default App

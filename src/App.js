2b3e286e5a4f7d8aa5a620aa458461393544bc34
mirlan21 <mirlan20010180@gmail.com>
2022-10-15 03:19:58 +0600

    create ui-select

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css'
-import UiSelect from './components/UI/UiSelect'
 
 function App() {
-   return (
-      <div className="App">
-         <UiSelect />
-      </div>
-   )
+   return <div className="App" />
 }
 
 export default App
--- src/components/UI/UiSelect.js
+++ src/components/UI/UiSelect.js
@@ -1,104 +1,60 @@
-import * as React from 'react'
-// import MenuItem from '@mui/material/MenuItem'
-// import { styled as styles } from '@mui/material/'
+import React from 'react'
+import Box from '@mui/material/Box'
 import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import SelectMui from '@mui/material/Select'
 import styled from 'styled-components'
 
-const UiSelect = () => {
-   const selectMenu = [
-      'Электроника',
-      'Одежда',
-      'Школа',
-      'Дом и сад',
-      'Обувь',
-      'Транспорт',
-   ]
-   const [age, setAge] = React.useState('')
-
+function UiSelect({
+   value,
+   width,
+   height,
+   onChange,
+   options,
+   getOptionLabel,
+   getOptionValue,
+}) {
    const handleChange = (event) => {
-      setAge(event.target.value)
+      onChange(event.target.value)
    }
 
    return (
-      <div>
-         <SelectContainer>
-            <Select value={age} onChange={handleChange}>
-               {selectMenu.map((item) => {
-                  return <CustomMenuItem value={item}>{item}</CustomMenuItem>
-               })}
-            </Select>
-         </SelectContainer>
-         <div>
-            {/* <SelectItem name="" id="">
-               {selectMenu.map((elem) => {
+      <BoxStyled width={width}>
+         <Form height={height} fullWidth>
+            <SelectMui onChange={handleChange} value={value}>
+               {options?.map((item) => {
                   return (
-                     <Items>
-                        <option value={elem}>{elem}\</option>
-                     </Items>
+                     <Options value={getOptionValue(item)} key={item.id}>
+                        {getOptionLabel(item)}
+                     </Options>
                   )
                })}
-            </SelectItem> */}
-         </div>
-      </div>
+            </SelectMui>
+         </Form>
+      </BoxStyled>
    )
 }
-
 export default UiSelect
 
-// const Items = styled.optgroup`
-//    width: 100px;
-//    height: 50px;
-//    border: 2px solid red;
-//    background-color: blue;
-//    padding: 10px;
-//    font-size: 40px;
-//    :hover {
-//       background-color: red;
-//    }
-// `
-// const SelectItem = styled.select`
-//    width: 400px;
-//    height: 40px;
-// `
-const CustomMenuItem = styled.li`
-   padding: 2px;
-   margin: 4px;
-   border-bottom: 1px solid grey;
-   :hover {
-      background: rgba(134, 57, 181, 0.4);
+const Options = styled(MenuItem)`
+   &.css-kk1bwy-MuiButtonBase-root-MuiMenuItem-root:hover {
+      background: rgba(134, 57, 181, 0.4) !important;
+   }
+   &.css-kk1bwy-MuiButtonBase-root-MuiMenuItem-root.Mui-selected {
+      background: rgba(134, 57, 181, 0.2) !important;
    }
 `
-const SelectContainer = styled(FormControl)`
-   width: 396px;
-   box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.16);
-   &.MuiButtonBase-root {
-      background-color: none;
-   }
-   &.Mui-selected:hover {
-      background: rgba(134, 57, 181, 0.4);
-   }
-   &.MuiMenuItem-root:hover {
-      background: rgba(134, 57, 181, 0.4);
+
+const Form = styled(FormControl)`
+   & > div {
+      height: ${(props) => props.height || '35px'};
    }
 `
-// const Listyle = styled(MenuItem)`
-//    :hover {
-//       background: rgba(134, 57, 181, 0.4);
-//    }
-//    &.Mui-selected {
-//       background: rgba(134, 57, 181, 0.2);
-//    }
-//    &.MuiButtonBase-root:hover {
-//       background: rgba(134, 57, 181, 0.4);
-//    }
-//    &.MuiTouchRipple-root:active {
-//       background: rgba(134, 57, 181, 0.4);
-//    }
-//    .MuiTouchRipple-root:active {
-//       background-color: red;
-//    }
-//    &.Mui-selected:hover {
-//       background: rgba(134, 57, 181, 0.4);
-//    }
-// `
+const BoxStyled = styled(Box)`
+   width: ${(props) => props.width || '396px'};
+   background: #ffffff;
+   border-radius: 2px;
+   & fieldset {
+      border: 1px solid #c4c4c4 !important;
+   }
+`

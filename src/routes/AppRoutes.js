f14a0f99d1d86ededc341bc40ff8296afbdb31a4
Bektur12 <kanybekovBektur@gmail.com>
2022-10-20 10:34:20 +0600

    added GuestRoutes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { ROLES } from '../utils/constants/general'
 import AdminRoutes from './AdminRoutes'
+import GuestRoutes from './GuestRoutes'
 import UserRoutes from './UserRoutes'
 
 function AppRoutes() {
    const role = 'ADMIN'
+
+   if (!role) {
+      return <GuestRoutes />
+   }
 
    return (
       <>
---
+++ src/routes/GuestRoutes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function GuestRoutes() {
+   return <div>GuestRoutes</div>
+}
+
+export default GuestRoutes

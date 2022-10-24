63f46e0e5302fead4848581609adce6e9886b7ef
Bektur12 <kanybekovBektur@gmail.com>
2022-10-24 13:27:53 +0600

    pull deve

---
+++ src/layout/Header.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// import iconsBell from '../assets/svg/iconsBell.svg'
+// import profileIcon from '../assets/svg/profileIcons.svg'
+// import openIcon from '../assets/svg/openIcon.svg'
+function Header({ isInput }) {
+   // searchSelect input not done, will add later///
+   return (
+      <StyledHeader>
+         <Container>
+            {isInput ? <SearchInput /> : <SearchInput />}
+            <RightSideContainer>
+               <BellIcon alt="alt" />
+               <Profile>
+                  <img alt="userIcon" />
+                  <span>Naruto Uzumaki</span>
+                  <img alt="open" />
+               </Profile>
+            </RightSideContainer>
+         </Container>
+      </StyledHeader>
+   )
+}
+export default Header
+const StyledHeader = styled.header`
+   width: 100%;
+   padding-left: 294px;
+   background: #ffffff;
+   box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.15);
+   position: fixed;
+`
+const Container = styled.div`
+   display: flex;
+   align-items: center;
+   padding: 20px 23px;
+   justify-content: space-between;
+   column-gap: 20px;
+`
+const Profile = styled.div`
+   display: flex;
+   align-items: center;
+   gap: 7px;
+   padding-right: 16.5px;
+   & span {
+      white-space: nowrap;
+   }
+`
+const RightSideContainer = styled.div`
+   display: flex;
+   align-items: center;
+   column-gap: 30px;
+`
+const BellIcon = styled.img`
+   align-self: flex-end;
+   position: relative;
+   bottom: 1px;
+`

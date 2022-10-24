a9955464e92df3e5c729b436cd173d5cc0726e35
Bektur12 <kanybekovBektur@gmail.com>
2022-10-24 13:14:37 +0600

    dev pull

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css'
-import Sidebar from './layout/SideBar'
 
 function App() {
-   const list = [
-      {
-         text: 'text',
-         path: '/',
-         id: '1',
-      },
-   ]
-   return (
-      <div className="App">
-         <Sidebar listData={list} />
-      </div>
-   )
+   return <div className="App"> </div>
 }
 
 export default App

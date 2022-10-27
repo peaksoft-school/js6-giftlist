de9876276d8984ec8f2dc75680d6e9f5bf3b52a4
mirlan21 <mirlan20010180@gmail.com>
2022-10-27 18:13:02 +0600

    Bread-crumbs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
+import { BreadCrumbs } from './routes/BreadCrumbs'
 
 function App() {
-   return <div className="App" />
+   return (
+      <div className="App">
+         hello3/
+         <BreadCrumbs />
+      </div>
+   )
 }
 
 export default App

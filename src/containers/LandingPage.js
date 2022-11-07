de93955ae572db73e769c1979b9135eb9052dd0e
Bektur12 <kanybekovBektur@gmail.com>
2022-11-07 12:53:57 +0600

    bagfx all

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import HomePage from './TopSection'
-// import { MainLendingPage } from './landingPage/MainLandingPage'
+import TopSection from './TopSection'
+import { MainLendingPage } from './landingPage/MainLandingPage'
 
 function LandingPage() {
    return (
       <>
-         <HomePage />
-         {/* <MainLendingPage /> */}
+         <TopSection />
+         <MainLendingPage />
       </>
    )
 }

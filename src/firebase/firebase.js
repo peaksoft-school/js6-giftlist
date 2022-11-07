891519af39eeee4d14496147c4ed8fb5e01ee367
Bektur12 <kanybekovBektur@gmail.com>
2022-11-07 18:12:58 +0600

    save change

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
    apiKey: 'AIzaSyD3PqQqTLlqzngzmIgbyFHen7Q4gjSDzMU',
    authDomain: 'auth-4bd68.firebaseapp.com',
@@ -13,5 +9,5 @@ const firebaseConfig = {
    appId: '1:936473324512:web:24cefadd589b1257535985',
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
+console.log(app)
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store/indexStore'
+import './firebase/firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 

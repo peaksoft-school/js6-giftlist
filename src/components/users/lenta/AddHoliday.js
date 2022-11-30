3cac2a764ec78878196474ff9657ff10a28fbfde
Bektur12 <kanybekovbektur22@gmail.com>
2022-11-30 10:35:33 +0600

    save change

--- src/components/users/lenta/AddHoliday.js
+++ src/components/users/lenta/AddHoliday.js
@@ -1,24 +1,22 @@
+import { Modal } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
-import Modal from '../../UI/modals/Modal'
 
 function AddHoliday({ isOpen, onClose }) {
    return (
-      <div>
-         <Modal isOpen={isOpen} onClose={onClose}>
-            <StyledDiv>
+      <Modal open={isOpen} onClose={onClose}>
+         <StyledDiv>
+            <div>
                <Title>На какой праздник хотите получить?</Title>
                <hr />
                <Option>Курбан айт</Option>
-            </StyledDiv>
-         </Modal>
-      </div>
+            </div>
+         </StyledDiv>
+      </Modal>
    )
 }
 
 export default AddHoliday
-
-const StyledDiv = styled('div')``
 
 const Title = styled('span')`
    font-family: 'Inter';
@@ -30,3 +28,16 @@ const Title = styled('span')`
 `
 
 const Option = styled('p')``
+
+const StyledDiv = styled('div')`
+   position: absolute;
+   left: 50%;
+   top: 50%;
+   transform: translate(-50%, -50%);
+   background-color: white;
+   outline: none;
+   background: #ffffff;
+   box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.16);
+   border-radius: 3px;
+   display: flex;
+`

fc918c5cc046941ee3b0944c9d58d4fe0cbfff32
Bektur12 <kanybekovbektur22@gmail.com>
2022-12-11 02:11:33 +0600

    bugfx

--- src/components/UI/card/WishCard.js
+++ src/components/UI/card/WishCard.js
@@ -61,6 +61,7 @@ export default WishCard
 const ContainerCard = styled.div`
    height: 250px;
    width: 349px;
+   object-fit: cover;
    border-radius: 8px;
    background-color: rgba(255, 255, 255, 1);
    border: 1px solid #ffffff;
